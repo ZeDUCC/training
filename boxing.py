3862cec9924472ddb16494e0e8a66dfdf3721d6d
janahan <janahansiva6@gmail.com>
2023-11-05 18:54:08 -0500

    added move numbers

--- boxing.py
+++ boxing.py
@@ -5,6 +5,17 @@ from tkinter import *
 
 moves = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "bob", "slip", "pull"]
 
+#1: jab
+#2: cross
+#3: lead hook
+#4: rear hook
+#5: lead uppercut
+#6: rear uppercut
+#7: left body hook
+#8: right body hook
+#9: body jab
+#10: body hook
+
 combos = list(itertools.permutations(moves, 5))
 
 top = tkinter.Tk()
